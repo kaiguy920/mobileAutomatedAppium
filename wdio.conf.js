19f7d14bd51509fd4a9662b27d613f0bd5e8bb76
kaiguy920 <smithkai920@gmail.com>
2022-10-06 17:18:12 -0400

    adds files to initial testing

--- wdio.conf.js
+++ wdio.conf.js
@@ -59,8 +59,8 @@ exports.config = {
     "appium:automationName": "UIAutomator2",
     // /Users/ksma/webdriverio-appium/app/android/ApiDemos-debug.apk
     // instead of direct path, using current working directory to relative path
-    "appium:app": path.join(process.cwd(), "./app/android/ApiDemos-debug.apk")
-    // "appium:autoGrantPermissions": true
+    "appium:app": path.join(process.cwd(), "./app/android/ApiDemos-debug.apk"),
+    "appium:autoGrantPermissions": true
     }],
     //
     // ===================
