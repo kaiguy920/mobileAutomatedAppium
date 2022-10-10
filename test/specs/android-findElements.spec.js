62d53876df5cc2a3a66b7670186f49e87d2e6b5f
kaiguy920 <smithkai920@gmail.com>
2022-10-10 15:30:06 -0400

    adds tests with xpath

--- test/specs/android-findElements.spec.js
+++ test/specs/android-findElements.spec.js
@@ -9,7 +9,7 @@ describe('Android Elements Tests', () => {
         await expect(actionBar).toBeExisting()
     });
 
-    it.only('Find element by class name', async () => {
+    it('Find element by class name', async () => {
         // find element by class name
         const className = await $('android.widget.TextView')
 
@@ -18,4 +18,20 @@ describe('Android Elements Tests', () => {
         // Assertion
         await expect(className).toHaveText("API Demos")
     });
+
+    it('Find element by XPath', async () => {
+        // xpath - (//tagname[@attribute=value])
+        await $('//android.widget.TextView[@content-desc="Alert Dialogs"]').click()
+
+        // find by resourceID
+        await $('//android.widget.Button[@resource-id="io.appium.android.apis:id/select_button"]').click()
+
+        // find by text
+        await $('//android.widget.TextView[@text="Command two"]').click()
+
+        // find by class - assertion
+        const textAssertion = await $('//android.widget.TextView')
+        await expect(textAssertion).toHaveText("You selected: 1 , Command two")
+    });
+
 });
