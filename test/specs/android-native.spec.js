6c5e0f6ce88162a11d9cb2ca6bf963743c96022c
kaiguy920 <smithkai920@gmail.com>
2022-10-12 17:11:21 -0400

    adds scripts to text scrolling vertically & horizontally

--- test/specs/android-native.spec.js
+++ test/specs/android-native.spec.js
@@ -1,4 +1,7 @@
 describe('Android Native Feature Tests', () => {
+    // ======================================================================
+    //                      ACCESSING A SCREEN/VIEW DIRECTLY
+    // ======================================================================
     it('Access an Activity directly', async () => {
         // access activity
         //driver.startActivity(<appPackage>, <appActivity>);
@@ -11,8 +14,11 @@ describe('Android Native Feature Tests', () => {
         // assertions
         await expect($('//*[@text="App/Alert Dialogs"]'))
     });
+    // ======================================================================
+    //                            DIALOG BOXES
+    // ======================================================================
 
-    it.only('Working with Dialog Boxes', async () => {
+    it('Working with Dialog Boxes', async () => {
 
         // access activity (or go directly to a certain page/screen view)
         //driver.startActivity(<appPackage>, <appActivity>);
@@ -43,4 +49,66 @@ describe('Android Native Feature Tests', () => {
         
     });
     
+    // ======================================================================
+    //                          VERTICAL SCROLLING
+    // ======================================================================
+    it('Vertical scrolling', async () => {
+        await $(('~App')).click()
+        await $(('~Activity')).click()
+
+        // scroll to the end (not stable if element gets moved)
+        // scrollToEnd(<maxSwipes>, <steps/speed>)
+        // await $('android=new UiScrollable(new UiSelector().scrollable(true)).scrollToEnd(1, 5)')
+
+        // scrollTextIntoView - more stable
+        await $('android=new UiScrollable(new UiSelector().scrollable(true)).scrollTextIntoView("Secure Surfaces")').click()
+
+        // await $(('~Secure Surfaces')).click() // <-- part of scrollToEnd
+
+        // assertion
+        await expect($('~Secure Dialog')).toExist()
+        
+    });
+
+    // ======================================================================
+    //                          HORIZONTAL SCROLLING
+    // ======================================================================
+    it('Horizontal scrolling', async () => {
+        // views --> gallery --> 1. photos
+        await driver.startActivity("io.appium.android.apis", ".view.Gallery1");
+
+        // Horizontal scrolling
+         await $('android=new UiScrollable(new UiSelector().scrollable(true)).setAsHorizontalList().scrollForward()')
+
+         await $('android=new UiScrollable(new UiSelector().scrollable(true)).setAsHorizontalList().scrollBackward()')
+
+         await driver.pause(3000)
+        
+    });
+
+
+    // ======================================================================
+    //                         SCROLLING EXERCISE
+    // ======================================================================
+    it.only('Working with a date picker', async () => {
+        // view --> Date Widgets --> Dialog
+        await driver.startActivity("io.appium.android.apis", ".view.DateWidgets1");
+
+        // get the current date
+        const date = await $('//*[@resource-id="io.appium.android.apis:id/dateDisplay"]')
+        const currentDate = await date.getText();
+
+        // click on "change the date"
+        await $('~change the date').click()
+        // scroll horizontally to the right
+        await $('android=new UiScrollable(new UiSelector().scrollable(true)).setAsHorizontalList().scrollForward()')
+        // pick the 10th date from the month
+        await $(`//*[@text="10"]`).click()
+        // click OK button
+        await $(`//*[@text="OK"]`).click()
+        // Assert the date is updated
+        await expect(await date.getText()).not.toEqual(currentDate)
+        
+    });
+
 });
