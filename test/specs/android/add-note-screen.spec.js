c080bb92f666b1ed70ec41310f447004a67cdecd
kaiguy920 <smithkai920@gmail.com>
2022-10-13 16:06:48 -0400

    adds screen objects to simplify code

---
+++ test/specs/android/add-note-screen.spec.js
@@ -0,0 +1,71 @@
+const AddNoteScreen = require("../screenobjects/android/add-notes")
+const EditNoteScreen = require("../screenobjects/android/edit-note")
+
+describe('Add Notes', () => {
+    it('Skip tutorial', async () => {
+        await AddNoteScreen.skipBtn.click()
+        // assertion
+        await expect(AddNoteScreen.addNoteTxt).toBeDisplayed()
+    });
+
+    // ===========================================================================
+    //                              ADD NOTE
+    // ===========================================================================
+
+    it('add a note, save changes & verify note', async () => {
+        await AddNoteScreen.addNoteTxt.click()
+        await AddNoteScreen.textOption.click()
+        await expect(AddNoteScreen.textEditing).toBeDisplayed()
+
+        // add note title
+        await AddNoteScreen.noteHeading.setValue("Fav Anime List")
+
+        // add note body
+        await AddNoteScreen.noteBody.setValue("Naruto\nOnePiece\nAOT")
+
+        // save the changes (click back twice)
+        await AddNoteScreen.saveNote()
+
+        // assertion
+        await expect(AddNoteScreen.editBtn).toBeDisplayed()
+
+        await expect (AddNoteScreen.viewNote).toHaveText("Naruto\nOnePiece\nAOT")
+
+    });
+
+    // ===========================================================================
+    //                              DELETE NOTE
+    // ===========================================================================
+    it.only('delete note', async () => {
+         // ======================== CREATE =================================
+        await EditNoteScreen.skipTutorial()
+        await EditNoteScreen.addAndSaveNote("TV Shows", "Friends\nBreakingBad\nThe Office")
+        // ======================== DELETE =================================
+        await driver.back()
+
+        const note = await EditNoteScreen.firstNote.getText()
+
+        // click on note
+        await EditNoteScreen.firstNote.click()
+
+        // click on more icon
+        await EditNoteScreen.moreIcon.click()
+
+        // click on delete item
+        await EditNoteScreen.deleteBtn.click()
+        await EditNoteScreen.okBtn.click()
+
+        // click on nav icon
+        await EditNoteScreen.navIcon.click()
+
+        // click trash can item
+        await EditNoteScreen.trashCanItem.click()
+
+        // ======================== VERIFY DELETE ===============================
+        // assertion
+        const trashCanItem = await EditNoteScreen.firstNote
+
+        await expect(trashCanItem).toHaveText(note)
+    });
+
+});
---
+++ test/specs/screenobjects/android/edit-note.js
@@ -0,0 +1,56 @@
+const AddNoteScreen = require("./add-notes")
+class EditNoteScreen {
+
+    get firstNote() {
+        return $('//*[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/title"]')
+    }
+
+    get moreIcon() {
+       return $('~More')
+    }
+
+    get deleteBtn() {
+        return $('//*[@text="Delete"]')
+    }
+
+    get okBtn() {
+        return $('//*[@text="OK"]')
+    }
+
+    get navIcon() {
+        return $('//*[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/icon_nav"]')
+    }
+
+    get trashCanItem() {
+        return $('//*[@text="Trash Can"]')
+    }
+
+    async skipTutorial() {
+        await AddNoteScreen.skipBtn.click()
+        // assertion
+        await expect(AddNoteScreen.addNoteTxt).toBeDisplayed()
+
+    }   
+
+    async addAndSaveNote(noteHeading, noteBody) {
+        await AddNoteScreen.addNoteTxt.click()
+        await AddNoteScreen.textOption.click()
+        await expect(AddNoteScreen.textEditing).toBeDisplayed()
+
+        // add note title
+        await AddNoteScreen.noteHeading.setValue(noteHeading)
+
+        // add note body
+        await AddNoteScreen.noteBody.setValue(noteBody)
+
+        // save the changes (click back twice)
+        await AddNoteScreen.saveNote()
+
+        // assertion
+        await expect(AddNoteScreen.editBtn).toBeDisplayed()
+
+        await expect (AddNoteScreen.viewNote).toHaveText(noteBody)
+    }
+}
+
+module.exports = new EditNoteScreen()
